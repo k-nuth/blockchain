f83909ef58f02bac81f52b9d16e2a2c62cdc4cfc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-08-03 10:07:10 -0300

    misc

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
                 and settings["arch"] == "x86_64" \
-                and options["bitprim-database:shared"] == False:
+                and options["bitprim-blockchain:shared"] == False:
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
