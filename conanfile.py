3c7e3c6288c3abc8d007ade398856dd690b819a6
dario_ramos <Dario.Ramos@tellago.com>
2017-08-16 17:22:09 -0300

    DEV-10: Use bitprim@bitprim user, testing channel, drop VS 2013 support, and MinGw x86 builds

--- conanfile.py
+++ conanfile.py
@@ -16,8 +16,8 @@ class BitprimblockchainConan(ConanFile):
     build_policy = "missing"
 
     requires = (("bitprim-conan-boost/1.64.0@bitprim/stable"),
-                ("bitprim-database/0.1@bitprim/stable"),
-                ("bitprim-consensus/0.1@bitprim/stable"))
+                ("bitprim-database/0.1@bitprim/testing"),
+                ("bitprim-consensus/0.1@bitprim/testing"))
 
     def build(self):
         cmake = CMake(self)
