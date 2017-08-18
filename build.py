751bfb60dfb138b64671249cd2ed76aa9ba2aeb4
dario_ramos <Dario.Ramos@tellago.com>
2017-08-18 16:47:47 -0300

    DEV-23: For VS, do only MT build

--- build.py
+++ build.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
         if settings["build_type"] == "Release" \
-                and not options["bitprim-blockchain:shared"]:
+                and not options["bitprim-blockchain:shared"] \
+                and (not "compiler.runtime" in settings or not settings["compiler.runtime"] == "MD"):
             filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
