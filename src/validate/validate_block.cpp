26037471fbc140426bd706c00d4388cb81063a8b
Ramiro Carlucho <ramirocarlucho@gmail.com>
2017-12-22 18:45:16 -0300

    Accept ebp block fix

--- src/validate/validate_block.cpp
+++ src/validate/validate_block.cpp
@@ -249,7 +249,8 @@ void validate_block::handle_accepted(const code& ec, block_const_ptr block,
         return;
     }
 
-    const auto exceeded = *sigops > get_max_block_sigops();
+    size_t allowed_sigops = block->is_ebp()? get_allowed_sigops(block->serialized_size(1)) : get_max_block_sigops(); 
+    const auto exceeded = *sigops > allowed_sigops;
     handler(exceeded ? error::block_embedded_sigop_limit : error::success);
 }
 
