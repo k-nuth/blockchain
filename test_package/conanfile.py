b641f1601a18d5144a91cda4c1a298e9ce257695
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-01-15 14:33:14 -0300

    Conan fix

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,7 +16,7 @@ class BitprimblockchainTestConan(ConanFile):
         cmake = CMake(self)
         cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = "ON"
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure(source_dir=self.source_folder, build_dir="./")
         cmake.build()
 
     def imports(self):
