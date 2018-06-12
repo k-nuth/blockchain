9397223fe1af791e5a491cba7755fe7f48855e5f
Guillermo <guillermo@bitprim.org>
2018-06-11 23:14:12 -0300

    Fix output index

--- src/interface/block_chain.cpp
+++ src/interface/block_chain.cpp
@@ -1070,7 +1070,6 @@ std::vector<libbitcoin::blockchain::mempool_transaction_summary> block_chain::ge
         size_t i = 0;
         for (auto const& output : tx.outputs()) {
             const auto tx_addresses = libbitcoin::wallet::payment_address::extract(output.script(), encoding_p2kh, encoding_p2sh);
-            ++i;
             for(const auto tx_address : tx_addresses) {
                 if (tx_address && addrs.find(tx_address) != addrs.end()) {
                     ret.push_back
@@ -1079,6 +1078,7 @@ std::vector<libbitcoin::blockchain::mempool_transaction_summary> block_chain::ge
                                       "", std::to_string(output.value()), i, tx_res.arrival_time()));
                 }
             }
+            ++i;
         }
         i = 0;
         for (auto const& input : tx.inputs()) {
