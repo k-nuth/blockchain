9c631649754f6693ea4fab09fa542af6ab5b998f
Ramiro Carlucho <ramirocarlucho@gmail.com>
2018-09-10 15:20:28 -0300

    [KEOK-59] fix repetead entries

--- include/bitcoin/blockchain/interface/safe_chain.hpp
+++ include/bitcoin/blockchain/interface/safe_chain.hpp
@@ -48,14 +48,16 @@ public:
     typedef handle1<chain::stealth_compact::list> stealth_fetch_handler;
     typedef handle2<size_t, size_t> transaction_index_fetch_handler;
 #ifdef WITH_KEOKEN
-    //TODO SHARED POINTER
     typedef std::function<void (const code&, const std::shared_ptr <std::vector <libbitcoin::transaction_const_ptr>> ) > keoken_history_fetch_handler;
     typedef std::function<void (const code&,  header_const_ptr, size_t,  const std::shared_ptr <std::vector <libbitcoin::transaction_const_ptr>> , uint64_t ) > block_keoken_fetch_handler;
     virtual void fetch_keoken_history(const short_hash& address_hash, size_t limit,
         size_t from_height, keoken_history_fetch_handler handler) const = 0;
 
     virtual void fetch_block_keoken(const hash_digest& hash, bool witness,
-    block_keoken_fetch_handler handler) const = 0;
+      block_keoken_fetch_handler handler) const = 0;
+
+    virtual void get_to_keo_transaction(libbitcoin::hash_digest hash,
+      std::shared_ptr<std::vector<transaction_const_ptr>> keoken_txs) const = 0;
 #endif
     typedef handle1<std::vector<hash_digest>> confirmed_transactions_fetch_handler;
 
