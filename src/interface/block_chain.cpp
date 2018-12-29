3d62c2e3f60ea03e360c414bfa566ed0fea95b9b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-12-29 18:41:07 -0300

    style fixes

--- src/interface/block_chain.cpp
+++ src/interface/block_chain.cpp
@@ -2162,9 +2162,9 @@ bool block_chain::stopped() const
 }
 
 #if defined(BITPRIM_WITH_MINING)
-    std::pair<std::vector<libbitcoin::mining::transaction_element>, uint64_t> block_chain::get_block_template() const{
-        return mempool_.get_block_template();
-    }
+std::pair<std::vector<libbitcoin::mining::transaction_element>, uint64_t> block_chain::get_block_template() const {
+    return mempool_.get_block_template();
+}
 #endif
 
 } // namespace blockchain
