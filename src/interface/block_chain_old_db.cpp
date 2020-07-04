d89a9df85070d4da8f071bd03645e20289bd4d1d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-07-03 22:57:00 -0300

    block_chain_old_db needed to be modernized

--- src/interface/block_chain_old_db.cpp
+++ src/interface/block_chain_old_db.cpp
@@ -50,14 +50,14 @@ void blockchain::for_each_transaction_non_coinbase(size_t from, size_t to, bool 
     while (from <= to) {
 
         if (stopped()) {
-            handler(error::service_stopped, 0, chain::transaction{});
+            handler(error::service_stopped, 0, domain::chain::transaction{});
             return;
         }
     
         auto const block_result = database_.blocks().get(from);
 
         if ( ! block_result) {
-            handler(error::not_found, 0, chain::transaction{});
+            handler(error::not_found, 0, domain::chain::transaction{});
             return;
         }
         KTH_ASSERT(block_result.height() == from);
