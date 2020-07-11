699cf7b7f5c91d0c126b118acb45c454dc8d8545
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-07-10 23:59:00 -0300

    mempool_v1_old needed to be modernized

--- include/kth/blockchain/mining/mempool_v1_old.hpp
+++ include/kth/blockchain/mining/mempool_v1_old.hpp
@@ -1038,7 +1038,7 @@ private:
         return true;
     }
 
-    error::error_code_t process_utxo_and_graph(chain::transaction const& tx, index_t node_index, node& new_node) {
+    error::error_code_t process_utxo_and_graph(domain::chain::transaction const& tx, index_t node_index, node& new_node) {
         //TODO: evitar tratar de borrar en el UTXO Local, si el UTXO fue encontrado en la DB
 
         auto it = hash_index_.find(tx.hash());
