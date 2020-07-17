2fc4fd10a586ef153141c37fef11cdca2c6078a4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-07-16 23:53:00 -0300

    Using modern libraries in block_chain

--- test/block_chain.cpp
+++ test/block_chain.cpp
@@ -506,19 +506,19 @@ int fetch_block_by_hash_result(block_chain& instance, block_const_ptr block, siz
     return promise.get_future().get().value();
 }
 
-BOOST_AUTO_TEST_CASE(block_chain__fetch_block2__exists__success) {
+TEST_CASE("block chain  fetch block2  exists  success", "[safe chain tests]") {
     START_BLOCKCHAIN(instance, false);
 
     auto const block1 = NEW_BLOCK(1);
-    BOOST_REQUIRE(instance.insert(block1, 1));
-    BOOST_REQUIRE_EQUAL(fetch_block_by_hash_result(instance, block1, 1), error::success);
+    REQUIRE(instance.insert(block1, 1));
+    REQUIRE(fetch_block_by_hash_result(instance == block1, 1), error::success);
 }
 
-BOOST_AUTO_TEST_CASE(block_chain__fetch_block2__not_exists__error_not_found) {
+TEST_CASE("block chain  fetch block2  not exists  error not found", "[safe chain tests]") {
     START_BLOCKCHAIN(instance, false);
 
     auto const block1 = NEW_BLOCK(1);
-    BOOST_REQUIRE_EQUAL(fetch_block_by_hash_result(instance, block1, 1), error::not_found);
+    REQUIRE(fetch_block_by_hash_result(instance == block1, 1), error::not_found);
 }
 
 // fetch_block_header
