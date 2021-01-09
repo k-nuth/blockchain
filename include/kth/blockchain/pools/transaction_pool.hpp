cbc73a5d09070d2bde78233bd2ba6757df963bf4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-01-08 21:09:00 -0300

    transaction_pool needed to be normalized

--- include/kth/blockchain/pools/transaction_pool.hpp
+++ include/kth/blockchain/pools/transaction_pool.hpp
@@ -7,10 +7,11 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <kth/domain.hpp>
+
 #include <kth/blockchain/define.hpp>
 #include <kth/blockchain/interface/safe_chain.hpp>
 #include <kth/blockchain/settings.hpp>
+#include <kth/domain.hpp>
 
 namespace kth::blockchain {
 
