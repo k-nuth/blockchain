ee8ee525626097cb6fcecc559e1e37dc1b4360c4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-04-05 18:11:37 +0200

    Use Conan Transitive Dependencies (#11)

--- conanfile.py
+++ conanfile.py
@@ -65,26 +65,9 @@ class KnuthBlockchainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.3.1")
 
     def requirements(self):
-        self.requires("infrastructure/0.23.0")
-        self.requires("domain/0.25.0")
-
-        self.requires("database/0.28.0")
+        self.requires("database/0.28.0", transitive_headers=True, transitive_libs=True)
         if self.options.consensus:
-            self.requires("consensus/0.23.0")
-
-        self.requires("boost/1.81.0")
-        self.requires("fmt/9.1.0")
-        self.requires("spdlog/1.11.0")
-
-        if not self._is_legacy_db():
-            if self.options.use_libmdbx:
-                self.requires("libmdbx/0.7.0@kth/stable")
-                self.output.info("Using libmdbx for DB management")
-            else:
-                self.requires("lmdb/0.9.29")
-                self.output.info("Using lmdb for DB management")
-        else:
-            self.output.info("Using legacy DB")
+            self.requires("consensus/0.23.0", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         KnuthConanFileV2.validate(self)
