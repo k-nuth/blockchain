554598e6a01e445dd36f4059c1a75c4b75bc37a8
Fernando Pelliccioni <fpelliccioni@gmail.com>
2023-04-07 16:20:51 +0200

    upgrade dependencies

--- conanfile.py
+++ conanfile.py
@@ -65,7 +65,7 @@ class KnuthBlockchainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.3.1")
 
     def requirements(self):
-        self.requires("database/0.29.0", transitive_headers=True, transitive_libs=True)
+        self.requires("database/0.30.0", transitive_headers=True, transitive_libs=True)
         if self.options.consensus:
             self.requires("consensus/0.24.0", transitive_headers=True, transitive_libs=True)
 
