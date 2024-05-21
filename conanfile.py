01787627c987ad7d374feba26678690797af95ec
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-05-21 14:02:01 +0200

    feat: bump dependencies

--- conanfile.py
+++ conanfile.py
@@ -62,7 +62,7 @@ class KnuthBlockchainConan(KnuthConanFileV2):
 
     def build_requirements(self):
         if self.options.tests:
-            self.test_requires("catch2/3.5.2")
+            self.test_requires("catch2/3.6.0")
 
     def requirements(self):
         self.requires("database/0.39.0", transitive_headers=True, transitive_libs=True)
