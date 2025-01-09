5dbdb9337b64571279a2d90ddf7d7ebf576675c4
Fernando Pelliccioni <fpelliccioni@gmail.com>
2025-01-09 18:46:29 +0100

    release v0.45.0

--- conanfile.py
+++ conanfile.py
@@ -66,9 +66,9 @@ class KnuthBlockchainConan(KnuthConanFileV2):
             self.test_requires("catch2/3.6.0")
 
     def requirements(self):
-        self.requires("database/0.46.0", transitive_headers=True, transitive_libs=True)
+        self.requires("database/0.45.0", transitive_headers=True, transitive_libs=True)
         if self.options.consensus:
-            self.requires("consensus/0.34.0", transitive_headers=True, transitive_libs=True)
+            self.requires("consensus/0.35.0", transitive_headers=True, transitive_libs=True)
 
     def validate(self):
         KnuthConanFileV2.validate(self)
